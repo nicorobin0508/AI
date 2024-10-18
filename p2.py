039d69ad7af310300950d47b929aeef556a6b8fa
nicorobin0508 <117674705+nicorobin0508@users.noreply.github.com>
2024-10-18 20:16:03 +0530

    Create p2.py

---
+++ p2.py
@@ -0,0 +1,46 @@
+global N
+N = 4
+
+def printSolution(board):
+    for i in range(N):
+        for j in range(N):
+            print(board[i][j], end=' ')
+        print()
+
+def isQSafe(board, row, col):
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    for i, j in zip(range(row, N, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    return True
+
+def solveNQUtil(board, col):
+    if col >= N:
+        return True
+    for i in range(N):
+        if isQSafe(board, i, col):
+            board[i][col] = 1
+            if solveNQUtil(board, col + 1) == True:
+                return True
+            board[i][col] = 0
+    return False
+
+def SolveNQ():
+    board = [[0, 0, 0, 0],
+             [0, 0, 0, 0],
+             [0, 0, 0, 0],
+             [0, 0, 0, 0]]
+    
+    if not solveNQUtil(board, 0):
+        print("Solution does not exist")
+        return False
+    
+    printSolution(board)
+    return True
+
+SolveNQ()
